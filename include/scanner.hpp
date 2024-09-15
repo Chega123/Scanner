0f1f258f5e27a77e8c15589c686400d043be304f
traintagger <99@mba.local>
2024-09-15 13:25:25 -0500

    01

---
+++ include/scanner.hpp
@@ -0,0 +1,10 @@
+#ifndef SCANNER_HPP
+#define SCANNER_HPP
+
+class scanner {
+private:
+
+public:
+};
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "include/scanner.hpp"
+
+int main() {
+
+
+  return 0;
+}
+
